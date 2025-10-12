a879a6638354eaf6dbb843ad94ef65569facd00e
princelungucell-43 <test@3schools.com>
2025-10-12 15:40:49 +0200

    Initial Commit

---
+++ C Programming - mini project 6/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C Programming - mini project 6
+//
+//  Created by user on 12/10/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
